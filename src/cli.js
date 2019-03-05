c9a3435e3720edec4ef7e9b35ea82103cf73868b
Jussi Vatjus-Anttila <jussiva@gmail.com>
2019-03-05 14:47:40 +0200

    eslint cleaning

--- src/cli.js
+++ src/cli.js
@@ -11,34 +11,34 @@ program
   .parse(process.argv);
 
 const fromStdin = () => {
-    process.stdin.resume();
-    process.stdin.setEncoding('utf8');
-    return Promise.resolve(new Puml(process.stdin));
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+  return Promise.resolve(new Puml(process.stdin));
 };
-const fromFile = (input) => Promise.resolve(Puml.fromFile(input));
+const fromFile = input => Promise.resolve(Puml.fromFile(input));
 
 let source;
 if (process.stdin.isTTY) {
-    if (program.input) {
-        console.log(`Reading file: ${program.input}`); // eslint-disable-line no-console
-        source = fromFile(program.input);
-    } else {
-        console.log('Reading puml from stdin..');
-        source = fromStdin();
-    }
-} else {
+  if (program.input) {
+    console.log(`Reading file: ${program.input}`); // eslint-disable-line no-console
+    source = fromFile(program.input);
+  } else {
+    console.log('Reading puml from stdin..'); // eslint-disable-line no-console
     source = fromStdin();
+  }
+} else {
+  console.log('Reading puml from stdin..'); // eslint-disable-line no-console
+  source = fromStdin();
 }
 
 source
-    .then((puml) => puml.generate(program.lang))
-    .then((output) => {
-        if(program.out === 'console') {
-            return output.print();
-        } else {
-            return output.save(program.output);
-        }
-    })
-    .then(() => {
-       console.log('Ready.'); // eslint-disable-line no-console
-    });
+  .then(puml => puml.generate(program.lang))
+  .then((output) => {
+    if (program.out === 'console') {
+      return output.print();
+    }
+    return output.save(program.output);
+  })
+  .then(() => {
+    console.log('Ready.'); // eslint-disable-line no-console
+  });
